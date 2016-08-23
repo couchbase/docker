4097ad13f63170e76c74ead0bdf017361278f839
Traun Leyden <traun.leyden@gmail.com>
2016-08-23 01:55:02 -0700

    Tell users where logs can be found (#51)

--- community/couchbase-server/4.1.0/scripts/entrypoint.sh
+++ community/couchbase-server/4.1.0/scripts/entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 [[ "$1" == "couchbase-server" ]] && {
-    echo "Starting Couchbase Server -- Web UI available at http://<ip>:8091"
+    echo "Starting Couchbase Server -- Web UI available at http://<ip>:8091 and logs available in /opt/couchbase/var/lib/couchbase/logs"
     exec /usr/sbin/runsvdir-start
 }
 
