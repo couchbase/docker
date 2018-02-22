a2f497d56fd1fe2df3ce8f72dc4521c289b40358
Ben Brooks <ben.brooks@couchbase.com>
2018-02-22 13:17:47 +0000

    Update to write directly to logfiles, rather than symlinks

--- community/sync-gateway/1.2.0-rc0/scripts/entrypoint.sh
+++ community/sync-gateway/1.2.0-rc0/scripts/entrypoint.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -e
 
-# Set up symlinks to log into a 'normal' location.
-mkdir -p /var/log/sync_gateway
-ln -sf /stdout.log /var/log/sync_gateway/sync_gateway_access.log
-ln -sf /stderr.log /var/log/sync_gateway/sync_gateway_error.log
+LOGFILE_DIR=/var/log/sync_gateway
+mkdir -p $LOGFILE_DIR
+
+LOGFILE_ACCESS=$LOGFILE_DIR/sync_gateway_access.log
+LOGFILE_ERROR=$LOGFILE_DIR/sync_gateway_error.log
 
 # Run SG and use tee to append stdout and stderr to separate logfiles
 # Process substitution described here: https://stackoverflow.com/a/692407
-exec sync_gateway "$@" > >(tee -a /stdout.log) 2> >(tee -a /stderr.log >&2)
+exec sync_gateway "$@" > >(tee -a $LOGFILE_ACCESS) 2> >(tee -a $LOGFILE_ERROR >&2)
